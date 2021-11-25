5b0c2d77d5dca07026737ce07d3bf9c63e164251
bcalou <bastien.calou@gmail.com>
2021-11-25 09:38:39 +0100

    Fix typos

--- 08-functional/01-mutation.py
+++ 08-functional/01-mutation.py
@@ -1,10 +1,10 @@
-# Une transformation map ne doit nécessairement conserver le type d'entrée.
+# Une transformation map ne doit pas nécessairement conserver le type d'entrée.
 # Ainsi, il est tout à fait possible de transformer un tableau d'entiers en un
 # tableau de booleen, ou un tableau de dictionnaires en un tableau de strings.
 
 # Grâce à map, transformez cette liste d'utilisateurs en tableau de strings du
 # type : ["John Doe is 28", "Jane Doe is 32", ...]. Stockez le résultat dans
-# une variable "user_informations".
+# une variable "user_information".
 
 # Attention : certains utilisateurs n'ont pas d'âge. Dans ce cas, n'ajoutez pas
 # la partie "is...".
@@ -68,4 +68,4 @@ users: list[User] = [
 
 
 
-print('\033[92m✓ OK' if user_informations == ["John Doe is 28", "Jane Doe is 32", "Allan Doe"] else '\033[91m❌KO')
+print('\033[92m✓ OK' if user_information == ["John Doe is 28", "Jane Doe is 32", "Allan Doe"] else '\033[91m❌KO')
